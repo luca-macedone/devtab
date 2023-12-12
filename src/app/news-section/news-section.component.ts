065df99aa8e1fb0ef740e332d1f8bf9e59d0f662
luca-macedone <luca.macedone@outlook.it>
2023-12-12 13:22:50 +0100

    converting news api to RSS feed news

--- src/app/news-section/news-section.component.ts
+++ src/app/news-section/news-section.component.ts
@@ -2,44 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 import { environment } from '../../environments/environment';
 
-interface Article {
-  source: {},
-  author: string,
-  title: string,
-  description: string,
-  url: string,
-  urlToImage: string,
-  publishedAt: string,
-  content: string,
-}
-
 @Component({
   selector: 'app-news-section',
   templateUrl: './news-section.component.html',
   styleUrl: './news-section.component.scss'
 })
 export class NewsSectionComponent implements OnInit {
-  news_api_base = `https://newsapi.org/v2/top-headlines`
+  // news_api_base = `https://newsapi.org/v2/top-headlines`
   // api_query = '?q=WebDevelopment'
   // from_date_query = `&from=${this.getYesterday()}`
-  sorting_query = '&sortBy=popularity'
-  key = `&apiKey=${environment.NEWS_API_KEY}`
+  // sorting_query = '&sortBy=popularity'
+  // key = `&apiKey=${environment.NEWS_API_KEY}`
   // category_query = '&categoty=technology'
-  country_query = '?country=it'
+  // country_query = '?country=it'
 
-  news: Array<Article> = []
+  news: Array<any> = []
 
   ngOnInit(): void {
+    // .get(`${this.news_api_base + this.country_query + this.sorting_query + this.key}`)
     axios
-      .get(`${this.news_api_base + this.country_query + this.sorting_query + this.key}`)
+      .get(`${environment.NEWS}`)
       .then(response => {
-        if (response.data.status == 'ok') {
-          this.news = response.data.articles
+        if (response) {
+          this.news = response.data.items
           // console.log(this.news)
         }
       })
       .catch(error => {
-        console.error(error.getMessage())
+        console.error(error)
       })
   }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    NEWS_API_KEY: 'bd120eb1f02b4a08b70d65e108f675ea',
+    // NEWS_API_KEY: 'bd120eb1f02b4a08b70d65e108f675ea',
+    NEWS: 'https://rss.app/feeds/v1.1/GSl8gBdJ7uWmdTKJ.json',
     QUIZ_API_KEY: 'hIwZAaqbkgtrmRJfShFMwBQDY3HM6Wo1h7uPPtTP',
 };
