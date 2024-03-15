9c23ccf4044bf29e2a6041e0d9f2fa0fcd582ce6
luca-macedone <luca.macedone@outlook.it>
2024-03-15 15:06:57 +0100

    add medium top news

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     // NEWS_API_KEY: 'bd120eb1f02b4a08b70d65e108f675ea',
-    NEWS: 'https://rss.app/feeds/v1.1/GSl8gBdJ7uWmdTKJ.json',
+    GOOGLE_TOP_NEWS: 'https://rss.app/feeds/v1.1/GSl8gBdJ7uWmdTKJ.json',
+    MEDIUM_TOP_NEWS: 'https://rss.app/feeds/v1.1/s4oPTJsQgP1FhFzB.json',
     QUIZ_API_KEY: 'hIwZAaqbkgtrmRJfShFMwBQDY3HM6Wo1h7uPPtTP',
 };
