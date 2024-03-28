154618258f8402e90a379438d5420577d6b0cea7
luca-macedone <luca.macedone@outlook.it>
2024-03-28 14:14:53 +0100

    new logo and new brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,30 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#2c7359",
+          dark: "#174031"
+        },
+        secondary: {
+          light: "#d2eae4",
+          dark: "#9bb5bf"
+        },
+        light: {
+          light: "#f2f2f2",
+          dark: "#d0d3d8"
+        },
+        dark: {
+          light: "#262626",
+          dark: "#0d0d0d"
+        },
+        muted: {
+          light: "#737373",
+          dark: "#404040"
+        }
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin'),
